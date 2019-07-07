9b01e480f0d0cc6e6c0b14141acafd45fbfdedb7
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-07 01:10:01 +0000

    Snapshot of branch master taken on 2019-07-06T21:10:00.442-04:00 from "https://github.com/UnofficialJuliaMirror/CurveProximityQueries.jl-e79eea86-5fe6-5310-9049-79d90d78ad74"

--- src/bounds.jl
+++ src/bounds.jl
@@ -1,23 +1,23 @@
 export upperbound, lowerbound
 
-function upperbound(a, b::Curve{D}, β::Interval) where {D}
+function upperbound(a, b::Curve{D, T}, β::Interval) where {D, T}
     evalpt = b(mid(β))
-    return minimum_distance(a, evalpt, @SVector(ones(D)))
+    return minimum_distance(a, evalpt, @SVector(fill(oneunit(T), D)))
 end
 
 function upperbound(a::Curve, b::Curve, α::Interval, β::Interval)
     norm(a(mid(α)) - b(mid(β)))
 end
 
-function lowerbound(a, b::Curve{D}, β::Interval) where {D}
+function lowerbound(a, b::Curve{D, T}, β::Interval) where {D, T}
     approx = cvxhull(b, β)
-    return minimum_distance(a, approx, @SVector(ones(D)))
+    return minimum_distance(a, approx, @SVector(fill(oneunit(T), D)))
 end
 
-function lowerbound(a::Curve{D}, b::Curve{D}, α::Interval, β::Interval) where {D}
+function lowerbound(a::Curve{D}, b::Curve{D, T}, α::Interval, β::Interval) where {D, T}
     bapprox = cvxhull(b, β)
     aapprox = cvxhull(a, α)
-    return minimum_distance(aapprox, bapprox, @SVector(ones(D)))
+    return minimum_distance(aapprox, bapprox, @SVector(fill(oneunit(T), D)))
 end
 
 function getbounds(a, b, θ...)
@@ -27,10 +27,14 @@ function getbounds(a, b, θ...)
 end
 
 import ConvexBodyProximityQueries: support
-support(pt::SVector{D, T}, dir::SVector{D, T}) where {D, N, T} = pt
-function support(vertices::SVector{N, SVector{D, T}}, dir::SVector{D, T}) where {D, N, T}
-    @inbounds vertices[argmax(Ref(dir').*vertices)]
-end
+support(pt::StaticVector{D}, dir::StaticVector{D}) where {D} = pt
+# the following two methods are both needed for method ambiguity resolution
+# given the method defined above, but both should do the same thing.
+support(vertices::SVector{N, <:StaticVector{D, T}}, dir::StaticVector{D}) where {D, N, T} =
+    _support(vertices, dir)
+support(vertices::SVector{D, <:StaticVector{D, T}}, dir::StaticVector{D}) where {D, T} =
+    _support(vertices, dir)
+_support(vertices, dir) = @inbounds vertices[argmax(Ref(dir').*vertices)]
 
 function cvxhull(b::Curve{2, T}, β::Interval) where {T}
     fA = b(β.lo)
@@ -38,12 +42,12 @@ function cvxhull(b::Curve{2, T}, β::Interval) where {T}
     center = (fA + fB)/2.0
     fdist = norm(fA - fB)
     rotate = SMatrix{2,2}(1.0I)
-    if fdist > 0.0
+    if fdist > zero(fdist)
         dir = (fB-fA)/fdist
         rotate = SMatrix{2,2}(dir[1],dir[2],-dir[2],dir[1])
     end
     a = arclength(b, β)/2.0
-    if (a - fdist/2.0) > eps(T)
+    if (a - fdist/2.0) > eps(T)*oneunit(T)
         b = sqrt(a^2 - (fdist/2)^2)
         octagon = SVector{8}(
                    SVector{2}(a*(√2 - 1), b),
@@ -67,7 +71,7 @@ function cvxhull(b::Curve{3, T}, β::Interval) where {T}
     center = (fA + fB)/2.0
     fdist = norm(fA - fB)
     rotate = SMatrix{3,3}(1.0I)
-    if fdist > 0.0
+    if fdist > zero(fdist)
         dir = (fB-fA)/fdist
         aux = SVector{3}(1.0, 0.0, 0.0)
         aux = abs(dir'*aux) < 0.99 ? aux : SVector{3}(0.0, 1.0, 0.0)
@@ -75,7 +79,7 @@ function cvxhull(b::Curve{3, T}, β::Interval) where {T}
         rotate = hcat(dir, aux, auxperp)
     end
     a = arclength(b, β)/2.0
-    if (a - fdist/2.0) > eps(T)
+    if (a - fdist/2.0) > eps(T)*oneunit(T)
         b = sqrt(a^2 - (fdist/2)^2)
         tetradecahedron = SVector{16}(
                    SVector{3}( a,  b*(√2 - 1),  b*(√2 - 1)),
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,49 @@ using LinearAlgebra
 using StaticArrays
 using IntervalArithmetic
 using Random: seed!
+using Unitful: nm, μm, mm, cm
 
 import CurveProximityQueries: differentiate, integrate
 
 @testset "CurveProximityQueries" begin
+    @testset "Constructors" begin
+        # should return SVector when constructed without SVectors
+        @test Bernstein([[0.0, 0.0], [1.0, 1.0]])(0.5) === @SVector([0.5, 0.5])
+
+        # non-floats arguments should become floats to satisfy assumptions
+        # elsewhere in the code (e.g. use of `eps(T)`)
+        @test eltype(Bernstein([[0, 0], [1, 1]])) <: Float64
+
+        # none of the following should error; some should be inferrable
+        @test Bernstein([
+                @SVector([0.0, 0.0]),
+                @SVector([1.0, 1.0])
+            ])(0.5) === @SVector([0.5, 0.5])
+        @test @inferred(Bernstein(@SVector([
+                @SVector([0.0, 0.0]),
+                @SVector([1.0, 1.0])
+            ])))(0.5) === @SVector([0.5, 0.5])
+        @test @inferred(Bernstein(
+                @SVector([0.0, 0.0]),
+                @SVector([1.0, 1.0])
+            ))(0.5) === @SVector([0.5, 0.5])
+
+        # similar reasoning below, but allow for any StaticVector
+        struct Point{T} <: StaticArrays.FieldVector{2, T}
+            x::T
+            y::T
+        end
+        StaticArrays.similar_type(::Type{P}, ::Type{T},
+            ::StaticArrays.Size{(2,)}) where {P <: Point, T} = Point{T}
+
+        @test Bernstein([Point(0.0, 0.0), Point(1.0, 1.0)])(0.5) ===
+            Point(0.5, 0.5)
+        @test @inferred(
+            Bernstein(@SVector([Point(0.0, 0.0), Point(1.0, 1.0)])))(0.5) ===
+                Point(0.5, 0.5)
+        @test @inferred(Bernstein(Point(0.0, 0.0), Point(1.0, 1.0)))(0.5) ===
+            Point(0.5, 0.5)
+    end
     @testset "Bernstein Polynomials" begin
         B2 = rand(Bernstein{2,8})
         B3 = rand(Bernstein{3,5})
@@ -58,5 +97,33 @@ import CurveProximityQueries: differentiate, integrate
         @test tolerance_verification(c, d, 0.1) == true
         @test tolerance_verification(c, d, 0.3) == false
         @test collision_detection(c, d) == false
+    end
+    @testset "Dimensionful curves" begin
+        b2 = Bernstein([[0.0, 1.0]μm, [1.0, 1.0]μm, [1.0, 0.0]μm])
+        @test b2(0.0) == [0, 1000]nm
+        @test b2(1.0) == [0.001, 0]mm
+        @test arclength(b2) == arclength(Bernstein([[0,1], [1,1], [1,0]])) * μm
+        @test length(CurveProximityQueries.cvxhull(b2, Interval(0.0, 1.0))) == 8
+
+        b3 = Bernstein([[0.0, 0.0, 0.0]mm, [1.0, 0.0, 0.0]mm, [1.0, 0.0, 1.0]mm])
+        c3 = Bernstein([[2.0, 1.0, 1.0]cm, [2.0, 3.0, 2.0]cm, [3.0, 1.0, 1.0]cm])
+        @test length(CurveProximityQueries.cvxhull(b3, Interval(0.0, 1.0))) == 16
+        @test abs(minimum_distance(b3, c3, atol=1e-8mm) - 2.328cm) ≤ 1e-4cm
+        @test tolerance_verification(b3, c3, 2cm, atol=1e-8mm) == true
+        @test collision_detection(b3, c3, atol=1e-8mm) == false
+    end
+    @testset "Support" begin
+        @test CurveProximityQueries.support(Point(1.0, 2.0), Point(1.0, 1.0)) ===
+            Point(1.0, 2.0)
+        @test CurveProximityQueries.support(@SVector(
+            [Point(0.0, 1.0), Point(1.0, 2.0)]), @SVector([1.0, 1.0])) ===
+                Point(1.0, 2.0)
+        @test CurveProximityQueries.support(@SVector(
+            [@SVector([0.0, 1.0]), @SVector([1.0, 2.0])]),
+                @SVector([1.0, 1.0])) === @SVector([1.0, 2.0])
+        @test CurveProximityQueries.support(@SVector(
+            [Point(0.0, 1.0), Point(1.0, 2.0), Point(3.0, 4.0)]), Point(1.0, 1.0)) ===
+                Point(3.0, 4.0)
+
     end
 end
